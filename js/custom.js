489a3ab81e87314ff28f95ce34430fd71ba5f712
abdulku1234 <abdulkuddus@gmail.com>
2023-06-20 20:44:09 -0700

    mkjjger

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+ let car1 = {
+    // property value
+     brand:"TATA",
+     year:"2023",
+     model:`Harrier`,
+    //function (many)
+
+    getFullDeatial(){
+        return this.brand
+    }
+ }
+
+ var car2 = {
+    // property value
+     brand:"TATA",
+     year:"2023",
+     model:`tiago.ev`,
+    //function (many)
+ }
+
+ 
+ const car3 = {
+    // property value
+     brand:"TATA",
+     year:"2023",
+     model:`Hexon`,
+    //function (many)
+ }
+ console.log(car1.getFullDeatial());
